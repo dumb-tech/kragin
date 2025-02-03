3bc66d43645a13d655a989e75d3c273bf41f0d82
Dima A. <mr.ebrilo@gmail.com>
2025-02-03 04:59:08 +0400

    added: debug method for modifier registerer changed: register receivers type changed to copy

--- registerer/modifier.go
+++ registerer/modifier.go
@@ -2,7 +2,10 @@ package registerer
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type ModifierFunc = func(map[string]any) func(any) (any, error)
@@ -14,45 +17,62 @@ type ModifierRegisterer struct {
 	modifiers  modifiersMap
 }
 
-func NewModifierRegisterer(pluginName string) *ModifierRegisterer {
-	reg := &ModifierRegisterer{pluginName: pluginName, modifiers: make(modifiersMap)}
+func NewModifierRegisterer(pluginName string) ModifierRegisterer {
+	reg := ModifierRegisterer{pluginName: pluginName, modifiers: make(modifiersMap)}
 	return reg
 }
 
-func (reg *ModifierRegisterer) AddModifier(name string, f ModifierFunc) {
+func (reg ModifierRegisterer) AddModifier(name string, f ModifierFunc) {
 	reg.modifiers[name] = f
 }
 
-func (reg *ModifierRegisterer) RegisterModifiers(f func(
+func (reg ModifierRegisterer) RegisterModifiers(f func(
 	name string,
 	factoryFunc func(map[string]any) func(any) (any, error),
 	appliesToRequest bool,
 	appliesToResponse bool,
 )) {
 	request, ok := reg.modifiers["request"]
-	if !ok {
-		f(reg.pluginName+"-request", request, true, false)
+	if ok {
+		name := reg.pluginName + "-request"
+		f(name, request, true, false)
 		delete(reg.modifiers, "request")
 	}
+
 	response, ok := reg.modifiers["response"]
-	if !ok {
-		f(reg.pluginName+"-response", response, false, true)
+	if ok {
+		name := reg.pluginName + "-response"
+		f(name, response, false, true)
 		delete(reg.modifiers, "response")
 	}
+
 	for name, modifier := range reg.modifiers {
 		f(reg.pluginName+"-"+name, modifier, true, true)
 	}
 }
 
+func (reg ModifierRegisterer) Debug() string {
+	sb := strings.Builder{}
+
+	sb.WriteString("Modifiers register for plugin: `" + reg.pluginName + "`\n")
+	sb.WriteString("Modifiers count: " + strconv.Itoa(len(reg.modifiers)))
+	for name, modifier := range reg.modifiers {
+		sb.WriteString(" - Modifier `" + name + "`: ")
+		sb.WriteString(fmt.Sprintf("%p\n", modifier))
+	}
+
+	return sb.String()
+}
+
 // Stubs
 
-func (reg *ModifierRegisterer) RegisterHandlers(f func(
+func (reg ModifierRegisterer) RegisterHandlers(f func(
 	name string,
 	handler func(context.Context, map[string]any, http.Handler) (http.Handler, error),
 )) {
 }
 
-func (reg *ModifierRegisterer) RegisterClients(f func(
+func (reg ModifierRegisterer) RegisterClients(f func(
 	name string,
 	handler func(context.Context, map[string]any) (http.Handler, error),
 )) {
